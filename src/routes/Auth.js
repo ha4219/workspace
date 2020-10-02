c6e4b69737b1ef5001039ed98ab8d63bc6633a33
동하 정 <ha4219@naver.com>
2020-10-02 14:33:05 +0000

    [ha4219] create new account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,36 +1,59 @@
 import React, { useState } from 'react';
+import { authService } from '../fbase';
 
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (e) => {
         const {target: {name, value}} = e;
+        console.log(name, value);
         if(name==="email"){
             setEmail(value);
         }else if(name==="password"){
             setPassword(value);
         }
     };
-    const onSubmit = (e) => {
+    const onSubmit = async(e) => {
         e.preventDefault();
+        try{
+            let data;
+            if(newAccount){
+                // create Account
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                );
+            }else{
+                // login
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        }catch(e){
+            console.log(e);
+        }
+        
     };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input type="text"
+                 name="email"
                  placeholder="Email" 
                  required 
                  value={email}
-                 onChange={onchange}
+                 onChange={onChange}
                  />
                 <input type="password" 
+                name="password"
                 placeholder="Password" 
                 required
-                value={email}
-                onChange={onchange}
+                value={password}
+                onChange={onChange}
                 />
-                <input type="submit" value="login"/>
+                <input type="submit" 
+                value={newAccount ? "Create Account" : "Login"}
+                />
             </form>
             <button>Continue with Google</button>
         </div>
