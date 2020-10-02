14a2388566c62ccb49303812a5f982300710cd8c
동하 정 <ha4219@naver.com>
2020-10-02 14:13:10 +0000

    [ha4219] login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React,{useState} from 'react';
-import AppRouter from './Router';
+import AppRouter from 'components/Router';
+import {authService} from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
 
   return (
-    <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+      <footer>&copy; donghaTwiiter {new Date().getFullYear()}</footer>
+    </>
+    
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => <div>
+    <form>
+        <input type="text" placeholder="Email" required />
+        <input type="password" placeholder="Password" required/>
+        <input type="submit" value="login"/>
+    </form>
+    <button>Continue with Google</button>
+</div>
 
 export default Auth;
