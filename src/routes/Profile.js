c6fad62470e5418bd0c72feca08c6664f80a5deb
동하 정 <ha4219@naver.com>
2020-10-05 14:50:17 +0000

    [ha4219] get tweet in profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,23 @@
-import { authService } from 'fbase';
-import React from 'react';
+import { authService, dbService } from 'fbase';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+    const getMyTweets = async () => {
+        const tweets = await dbService.collection("tweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt").get();
+
+    };
+    useEffect(() => {
+        getMyTweets();
+    }, []);
     return (
         <>
             <span>Profile</span>
